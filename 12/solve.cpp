19cb6138954070952f2e0d289f881cf0fc13dbdc
Yazid <108950946+yazid-k@users.noreply.github.com>
2025-09-29 20:48:15 +0200

    day14p2

--- 12/solve.cpp
+++ 12/solve.cpp
@@ -4,6 +4,76 @@
 #include <map>
 #include <vector>
 
+int get_number_of_sides(const std::set<std::pair<int,int>>& patch)
+{
+	std::set<std::tuple<int,int,int>> visited;
+	int sides = 0;
+
+	for (auto [x, y] : patch) {
+		if (patch.find({x, y-1}) == patch.end())
+		{
+			if (visited.find({x, y, 0}) == visited.end())
+			{
+				sides++;
+				int left = x, right = x;
+				while (patch.find({left-1, y}) != patch.end() && patch.find({left-1, y-1}) == patch.end())
+					left--;
+				while (patch.find({right+1, y}) != patch.end() && patch.find({right+1, y-1}) == patch.end())
+					right++;
+				for (int i = left; i <= right; i++)
+					visited.insert({i, y, 0});
+			}
+		}
+
+		if (patch.find({x, y+1}) == patch.end())
+		{
+			if (visited.find({x, y+1, 0}) == visited.end())
+			 {
+				sides++;
+				int left = x, right = x;
+				while (patch.find({left-1, y}) != patch.end() && patch.find({left-1, y+1}) == patch.end())
+					left--;
+				while (patch.find({right+1, y}) != patch.end() && patch.find({right+1, y+1}) == patch.end())
+					right++;
+				for (int i = left; i <= right; i++)
+					visited.insert({i, y+1, 0});
+			}
+		}
+
+		if (patch.find({x-1, y}) == patch.end())
+		{
+			if (visited.find({x, y, 1}) == visited.end())
+			{
+				sides++;
+				int top = y, bottom = y;
+				while (patch.find({x, top-1}) != patch.end() && patch.find({x-1, top-1}) == patch.end())
+					top--;
+				while (patch.find({x, bottom+1}) != patch.end() && patch.find({x-1, bottom+1}) == patch.end())
+					bottom++;
+				for (int i = top; i <= bottom; i++)
+					visited.insert({x, i, 1});
+			}
+		}
+
+		if (patch.find({x+1, y}) == patch.end())
+		{
+			if (visited.find({x+1, y, 1}) == visited.end())
+			{
+				sides++;
+				int top = y, bottom = y;
+				while (patch.find({x, top-1}) != patch.end() && patch.find({x+1, top-1}) == patch.end())
+					top--;
+				while (patch.find({x, bottom+1}) != patch.end() && patch.find({x+1, bottom+1}) == patch.end())
+					bottom++;
+				for (int i = top; i <= bottom; i++)
+					visited.insert({x+1, i, 1});
+			}
+		}
+	}
+
+	return sides;
+}
+
 int get_patch_perimeter(const std::set<std::pair<int,int>>& patch, std::vector<std::string>& grid)
 {
 	int perimeter = 0;
@@ -49,6 +119,7 @@ int main(int argc, char const *argv[])
 	while (std::getline(infile, line))
 		grid.push_back(line);
 	int total_price = 0;
+	int total_price_p2 = 0;
 	std::set<std::pair<int,int>> visited;
 	for (int i = 0; i < grid.size(); i++)
 	{
@@ -57,11 +128,13 @@ int main(int argc, char const *argv[])
 			if (visited.count({i,j})) continue;
 			std::set<std::pair<int,int>> patch_plants = get_patch(grid, i, j, grid[i][j], visited);
 			int perimeter = get_patch_perimeter(patch_plants, grid);
-			std::cout << "Patch of plant type " << grid[i][j] << " with size " << patch_plants.size() << " has perimeter " << perimeter << "\n";
+			int sides = get_number_of_sides(patch_plants);
 			total_price += patch_plants.size() * perimeter;
+			total_price_p2 += patch_plants.size() * sides;
 		}
 	}
 	std::cout << "Total price: " << total_price << "\n";
+	std::cout << "Total price (sides method): " << total_price_p2 << "\n";
 	return 0;
 }
 
